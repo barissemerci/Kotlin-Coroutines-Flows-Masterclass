e42505656b6270188bc749e197eb330ad090dc98
Barış SEMERCİ <barissemerci3@gmail.com>
2024-12-30 18:59:32 +0300

    init

---
+++ app/src/main/java/com/barissemerci/kotlincoroutinesflowsmasterclass/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.barissemerci.kotlincoroutinesflowsmasterclass
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.barissemerci.kotlincoroutinesflowsmasterclass.ui.theme.KotlinCoroutinesFlowsMasterclassTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinCoroutinesFlowsMasterclassTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinCoroutinesFlowsMasterclassTheme {
+        Greeting("Android")
+    }
+}
